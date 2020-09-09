846c7baa131256798328153f8b42c81c3723bd45
Michelle Johnson <Michelle.Johnson@domain.local>
2020-09-08 22:34:52 -0700

    Flask routes (9.5.2)

--- app.py
+++ app.py
@@ -1,7 +1,49 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Setup Database
+# Access database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect database
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Set class variables
+Measurement = Base.classes.measurment
+Station = Base.classes.station
+
+# Create session link
+session = Session(engine)
+
+# Setup Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# Welcome route
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+       filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
